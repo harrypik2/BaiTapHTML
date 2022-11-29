af1e6495dd4a33c655621b8c69026d9e6dd58be9
harrypik2 <ssharry2k3ss@gmail.com>
2022-11-29 11:49:09 +0700

    Update Br1.php

--- 29-11-2022/Br1.php
+++ 29-11-2022/Br1.php
@@ -1,11 +1,11 @@
 <?php
-$user = $_POST['user'];
-$pass = $_POST['pass'];
+// Kiểm tra 1 biến có tồn tại isset
+if(!isset($_POST['user']) or !isset($_POST['pass']) ){
+    die("Phải nhập nội dung từ index.php");
+}
 ?>
 
-<!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,19 +16,28 @@ $pass = $_POST['pass'];
             height: 280px;
             width: 300;
             border: 1px solid black;
+            position: relative;
             margin: auto;
-            text-align: center;
         }
-        h2{
-            vertical-align: middle;
+        #b{
+            color: red; 
+            position: absolute;
+            bottom: 3px;
+            left: 35px;
+        }
+        #h{
+            position: absolute;
+            top: 80px;
+            left: 83px;
+
         }
     </style>
 </head>
 
 <body>
     <div id="khung">
-        <h2>WELCOME</h2>
-        <div style="color: red; vertical-align: bottom;">Đăng nhập thành công lúc <span><?php date_default_timezone_set('Asia/Ho_Chi_Minh'); 
+        <h2 id="h">WELCOME</h2>
+        <div id="b">Đăng nhập thành công lúc <span><?php date_default_timezone_set('Asia/Ho_Chi_Minh'); 
     $t = date("H:i:s");
     echo $t;?></span></div>
     </div>
